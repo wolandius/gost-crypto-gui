e88c58a2e6ab536ad5e5923995ecb75741eb0f86
wolandius <wolandius@github.com>
2023-05-26 10:44:07 +0300

    v2.0 - added appimage support - added translations for app - port gostcryptogui-caja to python3

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -17,17 +17,11 @@ rsync -ah --progress ./gostcryptogui-$1.tar.gz ~/rpmbuild/SOURCES/
 
 rm -rf ~/rpmbuild/SPECS/gostcryptogui.spec
 
-#cp spec\ for\ 7.2/gostcryptogui.spec ~/rpmbuild/SPECS/
-#rpmbuild -ba ~/rpmbuild/SPECS/gostcryptogui.spec
-
-#rsync -ah --progress ~/rpmbuild/RPMS/noarch/gostcryptogui-$1-1.el7.2.noarch.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-$1-1.el7.2.noarch.rpm
-#rsync -ah --progress ~/rpmbuild/SRPMS/gostcryptogui-$1-1.el7.2.src.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-$1-1.el7.2.src.rpm
-
 cp spec\ for\ 7.3/gostcryptogui.spec ~/rpmbuild/SPECS/
 rpmbuild -ba ~/rpmbuild/SPECS/gostcryptogui.spec
 
 rm -rf gostcryptogui-$1.tar.gz gostcryptogui-$1
 
-rsync -ah --progress ~/rpmbuild/RPMS/noarch/gostcryptogui-$1-1.el7.3.noarch.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-$1-1.el7.3.noarch.rpm
-rsync -ah --progress ~/rpmbuild/SRPMS/gostcryptogui-$1-1.el7.3.src.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-$1-1.el7.3.src.rpm
-rsync -ah --progress ~/rpmbuild/RPMS/noarch/gostcryptogui-caja-$1-1.el7.3.noarch.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-caja-$1-1.el7.3.noarch.rpm
+rsync -ah --progress ~/rpmbuild/RPMS/noarch/gostcryptogui-$1-1.el7.*.noarch.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-$1-1.el7.*.noarch.rpm
+rsync -ah --progress ~/rpmbuild/SRPMS/gostcryptogui-$1-1.el7.*.src.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-$1-1.el7.*.src.rpm
+rsync -ah --progress ~/rpmbuild/RPMS/noarch/gostcryptogui-caja-$1-1.el7.*.noarch.rpm ~/git_projects/gost-crypto-gui/gostcryptogui-caja-$1-1.el7.*.noarch.rpm
